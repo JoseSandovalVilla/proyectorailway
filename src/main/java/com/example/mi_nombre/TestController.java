687ab0a7794781bdfcefc3003b5410c1c2875edd
JoseSandovalVilla <jsvilla776@gmail.com>
2024-07-31 07:55:51 -0600

    api dice nombre

---
+++ src/main/java/com/example/mi_nombre/TestController.java
@@ -0,0 +1,15 @@
+package com.example.mi_nombre;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class TestController {
+
+    @GetMapping("/hola/{nombre}")
+    public String obtenerNombre(@PathVariable String nombre) {
+        return "hola, "+ nombre + "!";
+    }
+}
